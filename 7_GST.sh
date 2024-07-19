93e6ecd8ee8fd7c68570984c351ca8c641f899a5
Prabaha Gangopadhyay <pg.prabaha@gmail.com>
2024-07-19 13:04:56 -0400

    remove .sh files from top folder

--- 7_GST.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --partition=verylong
-#SBATCH --time=168:00:00
-#SBATCH --job-name=PTP8_01102019_accg_GenShuffles
-#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=28
-#SBATCH --mem-per-cpu=8192
-#SBATCH --mail-type=ALL
-module load MATLAB/2017a
-matlab -nodisplay -nosplash -r "run('pdScripts/GST_01102019_accg.m');"
