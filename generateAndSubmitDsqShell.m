94b3e289be521e4d1da5527982f23579dc785827
Prabaha <prabaha.gangopadhyay@yale.edu>
2024-07-19 11:22:01 -0400

    add text file array job subset

--- generateAndSubmitDsqShell.m
+++ generateAndSubmitDsqShell.m
@@ -8,7 +8,7 @@ function generateAndSubmitDsqShell(jobFilePath)
     
     % Generate the dsq batch file using dsq
     dsqBatchFilePath = fullfile(pwd, 'pdScripts', 'dsq_batchfile.sh');
-    system(sprintf('bash -c "module load dSQ; module load miniconda; dsq --job-file %s --batch-file %s -o %s --status-dir %s --partition psych_week --cpus-per-task 28 --mem-per-cpu 2048 --time 7-00:00:00 --mail-type ALL"', ...
+    system(sprintf('bash -c "module load dSQ; module load miniconda; dsq --job-file %s --batch-file %s -o %s --status-dir %s --partition psych_week --cpus-per-task 48 --mem-per-cpu 2048 --time 7-00:00:00 --mail-type ALL"', ...
                    jobFilePath, dsqBatchFilePath, outputDir, outputDir));
 
     % Submit the dsq batch file using sbatch
